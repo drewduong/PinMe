a73a0209165da90bfdbba4a910d2b8290fda53a6
Andrew Duong <itsandrewduong@gmail.com>
2023-01-29 23:46:33 -0800

    Created a component for discover page, rendering all pins

---
+++ react-app/src/components/AllPins/index.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getPinsThunk } from '../../store/pins';
+import { NavLink } from 'react-router-dom';
+import './AllPins.css';
+
+const AllPins = () => {
+  const dispatch = useDispatch()
+
+  const pins = useSelector(state => Object.values(state.pins))
+  console.log('Get all pins state (useSelector)', pins)
+
+  useEffect(() => {
+    dispatch(getPinsThunk())
+  }, [dispatch])
+
+  if (!pins) return (<h1>No pins found</h1>)
+
+  return (
+    <div className='pins-container'>
+      <div className='pins-new'>
+        <button className='new-pin-button'>
+          <NavLink className='new-pin-link' to={'/pins/create'}><i class="fa-solid fa-plus"></i></NavLink>
+        </button>
+      </div>
+      {pins.map(pin => (
+        <div className='pins-item'>
+          <NavLink to={`/pins/${pin.id}`}>
+            <img className='pins-image' src={pin.pin_image} alt='No Preview' />
+          </NavLink>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default AllPins
