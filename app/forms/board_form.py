d6fed12ad26c87ce721f13f07f3c114c02d04e34
Andrew Duong <itsandrewduong@gmail.com>
2023-02-02 11:25:23 -0800

    Added validations for image url

--- app/forms/board_form.py
+++ app/forms/board_form.py
@@ -4,7 +4,20 @@ from wtforms.validators import DataRequired, ValidationError
 from app.models import db, User, Board
 
 
+def image_check(field):
+    url = field.data
+
+    if '.png' not in url and '.jpg' not in url and '.jpeg' not in url:
+        raise ValidationError(
+            'Invalid image url. Valid types include ".png, .jpg, or .jpeg"')
+
+    if not url.startswith('https://') and not url.startswith('http://'):
+        raise ValidationError(
+            'Invalid image url. Must begin with "http://" or "https://"')
+
+
 class BoardForm(FlaskForm):
-    name = StringField("Name")
-    board_image = StringField("Board Preview Image")
-    user_id = IntegerField("User Id")
+    name = StringField("Name", validators=[DataRequired()])
+    board_image = StringField("Board Preview Image", validators=[
+                              DataRequired(), image_check])
+    user_id = IntegerField("User Id", validators=[DataRequired()])
