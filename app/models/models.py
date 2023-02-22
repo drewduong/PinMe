6a96140bc70ea72c7fcf06d3c7650c043d16e19d
Andrew Duong <itsandrewduong@gmail.com>
2023-02-22 12:50:41 -0800

    Changed user model to include user info

--- app/models/models.py
+++ app/models/models.py
@@ -20,6 +20,9 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(40), nullable=False, unique=True)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    about = db.Column(db.String(255))
 
     boards = db.relationship('Board', back_populates='user')
     pins = db.relationship('Pin', back_populates='user')
@@ -45,6 +48,9 @@ class User(db.Model, UserMixin):
             'id': self.id,
             'username': self.username,
             'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'about': self.about,
             # Add pins and board
             'boards': [board.name for board in self.boards],
             'pins': [pin.title for pin in self.pins]
