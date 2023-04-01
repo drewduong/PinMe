c74a549e46f377d0ed8200863f8c8d5abb9fea1c
Andrew Duong <itsandrewduong@gmail.com>
2023-04-01 13:36:34 -0700

    Added to sessions store

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,6 +1,9 @@
 // constants
 const SET_USER = 'session/SET_USER';
 const REMOVE_USER = 'session/REMOVE_USER';
+const FOLLOW = 'session/FOLLOW'
+const UNFOLLOW = 'session/UNFOLLOW'
+const UPDATE_PROFILE = 'session/UPDATE_PROFILE'
 
 const setUser = (user) => ({
   type: SET_USER,
@@ -10,6 +13,33 @@ const setUser = (user) => ({
 const removeUser = () => ({
   type: REMOVE_USER,
 })
+
+// Follow a user
+const followAction = (user) => {
+  return {
+    type: FOLLOW,
+    user
+  }
+}
+
+// Unfollow a user
+const unfollowAction = (user) => {
+  return {
+    type: UNFOLLOW,
+    user
+  }
+}
+
+// Update profile - payload contains updated user details and userId
+const updateProfileAction = (user) => {
+  // console.log("Update a user profile (action):", user)
+  return {
+    type: UPDATE_PROFILE,
+    user
+  }
+}
+
+
 
 const initialState = { user: null };
 
@@ -24,7 +54,7 @@ export const authenticate = () => async (dispatch) => {
     if (data.errors) {
       return;
     }
-  
+
     dispatch(setUser(data));
   }
 }
@@ -40,8 +70,8 @@ export const login = (email, password) => async (dispatch) => {
       password
     })
   });
-  
-  
+
+
   if (response.ok) {
     const data = await response.json();
     dispatch(setUser(data))
@@ -82,7 +112,7 @@ export const signUp = (username, email, password) => async (dispatch) => {
       password,
     }),
   });
-  
+
   if (response.ok) {
     const data = await response.json();
     dispatch(setUser(data))
@@ -97,12 +127,74 @@ export const signUp = (username, email, password) => async (dispatch) => {
   }
 }
 
+export const followThunk = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/users/${userId}/follow`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(userId)
+  })
+  if (response.ok) {
+    const data = await response.json()
+    dispatch(followAction(data))
+    return data
+  }
+}
+
+export const unfollowThunk = (userId) => async (dispatch) => {
+  const response = await fetch(`/api/users/${userId}`, {
+    method: 'DELETE'
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    dispatch(unfollowAction(userId))
+    return data
+  }
+}
+
+export const updateProfileThunk = (user, userId) => async (dispatch) => {
+  // console.log("Update a user input payload (thunk):", userId)
+  const response = await fetch(`/api/users/${Number(userId)}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+
+  if (response.ok) {
+    const data = await response.json()
+    // console.log("Update a user profile backend data (thunk)", data)
+    dispatch(updateProfileAction(data))
+    return data
+  }
+}
+
+
 export default function reducer(state = initialState, action) {
   switch (action.type) {
     case SET_USER:
       return { user: action.payload }
     case REMOVE_USER:
       return { user: null }
+    case FOLLOW: {
+      const newState = { ...state }
+      newState[action.user.id] = action.user
+      return newState
+    }
+    case UNFOLLOW: {
+      const newState = { ...state }
+      delete newState[action.userId]
+      return newState
+    }
+    case UPDATE_PROFILE: {
+      const newState = { ...state }
+      newState[action.user.id] = action.user
+      // console.log('Update user (reducer)', newState)
+      return newState
+    }
     default:
       return state;
   }
